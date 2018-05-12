35100e11ae035d7eca09de998ae1bbd5f84d0a3e
dio <dio@getty.io>
2018-05-12 13:36:18 -0300

    change labels

--- src/routes/Dashboard/Monitor.js
+++ src/routes/Dashboard/Monitor.js
@@ -1,6 +1,6 @@
 import React, { PureComponent, Fragment } from 'react';
 import { connect } from 'dva';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, List } from 'antd';
 import ActiveChart from 'components/ActiveChart';
 import Authorized from '../../utils/Authorized';
 
@@ -33,8 +33,28 @@ export default class Monitor extends PureComponent {
             </Card>
           </Col>
           <Col xl={6} lg={24} md={24} sm={24} xs={24}>
-            <Card title="活动情况预测" style={{ marginBottom: 24 }} bordered={false}>
-              <ActiveChart />
+            <Card title="TRX Balance" style={{ marginBottom: 24 }} bordered={false}>
+              <p>1999.0000</p>
+            </Card>
+          </Col>
+        </Row>
+        <Row gutter={24}>
+          <Col xl={18} lg={24} md={24} sm={24} xs={24} style={{ marginBottom: 24 }}>
+            <Card title="TOKENS" style={{ marginBottom: 24 }} bordered={false}>
+              <List.Item key="..........">
+                <List.Item.Meta
+                  title={<a href="https://ant.design">TRX</a>}
+                  description="dio@getty.io"
+                />
+                <div>Content</div>
+              </List.Item>
+            </Card>
+          </Col>
+          <Col xl={6} lg={24} md={24} sm={24} xs={24}>
+            <Card title="Frozen Tokens" style={{ marginBottom: 24 }} bordered={false}>
+              <p>Card content</p>
+              <p>Card content</p>
+              <p>Card content</p>
             </Card>
           </Col>
         </Row>
