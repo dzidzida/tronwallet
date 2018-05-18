6b1bd222b3483cef6998871f26ebf4ad7b9dc303
dio <dio@getty.io>
2018-05-18 17:58:48 -0300

    improve dashboard

--- src/routes/Dashboard/Monitor.js
+++ src/routes/Dashboard/Monitor.js
@@ -309,33 +309,6 @@ class Monitor extends PureComponent {
           </Col>
           <Col xl={6} lg={24} md={24} sm={24} xs={24}>
             <Card
-              title="ENTROPY"
-              style={{ marginBottom: 30 }}
-              bordered={false}
-              extra={
-                <CopyToClipboard text={this.formatAmount(entropy)}>
-                  <Button type="primary" size="default" icon="copy" shape="circle" ghost />
-                </CopyToClipboard>
-              }
-            >
-              <ChartCard
-                bordered={false}
-                title="TRX "
-                total={this.formatAmount(entropy)}
-                footer={<span>{tronAccount}</span>}
-                contentHeight={46}
-              />
-            </Card>
-          </Col>
-        </Row>
-        <Row gutter={24}>
-          <Col xl={6} lg={24} md={24} sm={24} xs={24}>
-            <Card title="TOKENS" style={{ marginBottom: 16 }} bordered={false}>
-              {this.renderTokens()}
-            </Card>
-          </Col>
-          <Col xl={6} lg={24} md={24} sm={24} xs={24} style={{ marginBottom: 16 }}>
-            <Card
               title="MY TRON ACCOUNT"
               style={{ marginBottom: 30 }}
               bordered={false}
@@ -363,9 +336,35 @@ class Monitor extends PureComponent {
                 footer={<Field label={moment(new Date()).format('dddd, MMMM Do YYYY')} />}
                 contentHeight={46}
               />
+            </Card>						
+          </Col>
+        </Row>
+        <Row gutter={24}>
+				
+          <Col xl={6} lg={24} md={24} sm={24} xs={24}>
+					<Card
+              title="ENTROPY"
+              style={{ marginBottom: 30 }}
+              bordered={false}
+              extra={
+                <CopyToClipboard text={this.formatAmount(entropy)}>
+                  <Button type="primary" size="default" icon="copy" shape="circle" ghost />
+                </CopyToClipboard>
+              }
+            >
+              <ChartCard
+                bordered={false}
+                title="TRX "
+                total={this.formatAmount(entropy)}
+                footer={<span>{tronAccount}</span>}
+                contentHeight={46}
+              />
+            </Card>
+            <Card title="TOKENS" style={{ marginBottom: 16 }} bordered={false}>
+              {this.renderTokens()}
             </Card>
           </Col>
-          <Col xl={6} lg={24} md={24} sm={24} xs={24} style={{ marginBottom: 16 }}>
+          <Col xl={12} lg={24} md={24} sm={24} xs={24} style={{ marginBottom: 16 }}>
             <Card title="TRANSACTIONS" style={{ marginBottom: 16 }} bordered={false}>
               {this.renderTransactions()}
             </Card>
