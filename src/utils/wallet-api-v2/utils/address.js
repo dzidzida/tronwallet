4558f9ef48348be923d0e0b149eefe917ffd5765
dio <dio@getty.io>
2018-05-18 22:26:57 -0300

    fix send

--- src/utils/wallet-api-v2/utils/address.js
+++ src/utils/wallet-api-v2/utils/address.js
@@ -1,20 +1,10 @@
-const ADDRESS_PREFIX = "a0";
-const ADDRESS_SIZE = 42;
+const ADDRESS_PREFIX = '27';
+const ADDRESS_SIZE = 35;
 
-
-export function isAddressValid(address) {
-
-  if (!address || address.length === 0) {
-    return false;
-  }
-
-  if (address.length !== ADDRESS_SIZE) {
-    return false;
-  }
-
-  if (address.substr(0, 2).toUpperCase() !== ADDRESS_PREFIX.toUpperCase()) {
-    return false;
-  }
-
-  return true;
-}
+const isAddressValid = address => {
+ if (!address || !address.length) return false;
+ if (address.length !== ADDRESS_SIZE) return false;
+ if (address.substr(0, 2).toUpperCase() !== ADDRESS_PREFIX.toUpperCase()) return false;
+ return true;
+};
+export default isAddressValid;
