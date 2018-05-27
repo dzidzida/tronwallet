fc74c4ffedbcd58bcd0b748165c2b6c6327166b5
dbuarque <dio@getty.io>
2018-05-27 17:09:20 -0300

    merge

--- .webpackrc.js
+++ .webpackrc.js
@@ -3,6 +3,9 @@ const path = require('path');
 export default {
   entry: 'src/index.js',
   extraBabelPlugins: [['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }]],
+  "extraBabelIncludes": [
+    "node_modules/antd"
+  ],
   env: {
     development: {
       extraBabelPlugins: ['dva-hmr'],
