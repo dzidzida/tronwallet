a7d7a4979afb9692aa8b537b4bc0fa09d255359c
Bertao <pedrobertao11@hotmail.com>
2018-05-28 00:27:09 -0300

    fixing to token

--- src/components/Contract/Transfer.js
+++ src/components/Contract/Transfer.js
@@ -4,6 +4,11 @@ import moment from 'moment';
 import styles from './Contract.less';
 import { formatAmount } from './util';
 
+const handleToken = (token, amount) => {
+  if (token !== 'TRX') return amount.toLocaleString();
+  else return formatAmount(amount);
+};
+
 export default ({ transaction }) => {
   return (
     <React.Fragment>
@@ -15,28 +20,28 @@ export default ({ transaction }) => {
             <div>From: {transaction.transferFromAddress}</div>
             <div>To: {transaction.transferToAddress}</div>
           </div>
-                }
+        }
         description={
           <div className={styles.address}>
             <div className={styles.itemFont}>
               {moment(transaction.timestamp).fromNow()}
             </div>
           </div>
-                }
+        }
       />
       <div>
         <div className={styles.itemFont}>
           {transaction.transferFromAddress === transaction.ownerAddress ?
-                        (`-${formatAmount(transaction.amount)}`) :
-                        (`+${formatAmount(transaction.amount)}`)}
+            (`-${handleToken(transaction.tokenName, transaction.amount)}`) :
+            (`+${handleToken(transaction.tokenName, transaction.amount)}`)}
           {' '}
-          TRX
+          {transaction.tokenName}
           {
             transaction.transferFromAddress === transaction.ownerAddress ? (
               <Icon type="arrow-down" style={{ marginLeft: 5, fontSize: 24, color: 'red' }} />
             ) : (
               <Icon type="arrow-up" style={{ marginLeft: 5, fontSize: 24, color: '#53d769' }} />
-            )
+              )
           }
         </div>
       </div>
