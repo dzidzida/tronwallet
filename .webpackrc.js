e578edc834a02b2b2f033528e677b3dd2207d9b7
dbuarque <dio@getty.io>
2018-06-02 14:29:08 -0300

    add bundle optimization

--- .webpackrc.js
+++ .webpackrc.js
@@ -1,4 +1,6 @@
+
 const path = require('path');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 export default {
   entry: 'src/index.js',
@@ -22,4 +24,19 @@ export default {
   disableDynamicImport: true,
   publicPath: '/',
   hash: true,
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+    ],
+    // Automatically split vendor and commons
+    // https://twitter.com/wSokra/status/969633336732905474
+    // https://medium.com/webpack/webpack-4-code-splitting-chunk-graph-and-the-splitchunks-optimization-be739a861366
+    splitChunks: {
+      chunks: 'all',
+      name: false,
+    },
+    // Keep the runtime chunk seperated to enable long term caching
+    // https://twitter.com/wSokra/status/969679223278505985
+    runtimeChunk: true,
+  },
 };
